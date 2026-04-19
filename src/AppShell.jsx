bc88358a4f433b31ec425ae4ac022fe624d1cc69
KPuneethHarshaNaik <kpuneethharshanaik@gmail.com>
2026-04-19 10:00:17 +0530

    Fix: add missing GoogleOAuthProvider wrapper to AppShell

--- src/AppShell.jsx
+++ src/AppShell.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
+
 export default function AppShell() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }

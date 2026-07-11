429477f7e53659ef49ccbee9f6aea1614c358cb4
KPuneethHarshaNaik <kpuneethharshanaik@gmail.com>
2026-07-11 18:46:45 +0530

    fix: load .env.local in mail server to support resend credentials in local development

--- server/invite-mail-server.js
+++ server/invite-mail-server.js
@@ -1,4 +1,12 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import fs from 'fs';
+dotenv.config();
+if (fs.existsSync('.env.local')) {
+  const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+  }
+}
 import express from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
